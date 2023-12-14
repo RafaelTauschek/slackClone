776eb670d7d657a5490d78b35154b6f7483cafcd
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-12-14 21:27:56 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiKey: "AIzaSyAZ4eMpwxl5WTIFD-Y9OdHEDRcv9-gAnPM",
+    authDomain: "slack-clone-19a0c.firebaseapp.com",
+    projectId: "slack-clone-19a0c",
+    storageBucket: "slack-clone-19a0c.appspot.com",
+    messagingSenderId: "139231253159",
+    appId: "1:139231253159:web:9109466346394168770182"
+};
