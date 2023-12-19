dc9e5cb3ade4e9e3e346dec82308967cf24748aa
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-12-19 18:17:28 +0100

    thread chat and thread component, some basic html & css

--- src/app/components/chats/direct-chat/direct-chat.component.ts
+++ src/app/components/chats/direct-chat/direct-chat.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ChatAreaComponent } from '../chat-area/chat-area.component';
 
 @Component({
   selector: 'app-direct-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, RouterOutlet, ChatAreaComponent],
   templateUrl: './direct-chat.component.html',
   styleUrl: './direct-chat.component.scss'
 })
--- src/app/components/main-page/thread/thread.component.ts
+++ src/app/components/main-page/thread/thread.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ThreadChatComponent } from '../../chats/thread-chat/thread-chat.component';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, RouterOutlet, ThreadChatComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
