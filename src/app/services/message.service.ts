168c376543879b771fc8d480f0e9840478a0e574
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-12-23 09:53:55 +0100

    user subscription

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import { User } from '../models/user.class';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MessageService {
+export class MessageService implements OnDestroy {
+  activeUserSubscription: Subscription;
+  activeUser: User[] = []
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.activeUserSubscription = this.userService.activeUserObservable$.subscribe((activeUser) => {
+      this.activeUser = activeUser;
+    })
+  }
+
+
+
+  ngOnDestroy(): void {
+    this.activeUserSubscription.unsubscribe();
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.class';
 import { FirebaseService } from './firebase.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  activeUser = new BehaviorSubject<User[]>([]);
+  activeUserObservable$ = this.activeUser.asObservable();
 
-  constructor(private firebaseService: FirebaseService) { }
 
-  
+  constructor(private firebaseService: FirebaseService) {
+
+  }
+
+
+
+  setActiveUser(user: User): void {
+    console.log('User to set', user);
+    this.activeUser.next([user]);
+    console.log('Updated user:', this.activeUser);
+  }
+
 }
